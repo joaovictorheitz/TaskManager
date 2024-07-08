16ae9a10d8f8a8b3b401018f4cf6194743d9f305
joaovictorguedes <joao.guedes@webmotors.com.br>
2024-07-08 19:04:10 -0300

    initial commit

---
+++ Project.swift
@@ -0,0 +1,31 @@
+import ProjectDescription
+
+let project = Project(
+    name: "TaskManager",
+    targets: [
+        .target(
+            name: "TaskManager",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.TaskManager",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["TaskManager/Sources/**"],
+            resources: ["TaskManager/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "TaskManagerTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.TaskManagerTests",
+            infoPlist: .default,
+            sources: ["TaskManager/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "TaskManager")]
+        ),
+    ]
+)
