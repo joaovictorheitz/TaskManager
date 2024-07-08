56a4beccc995af52b1a14ed5838fcfd55daabe18
joaovictorguedes <vcitorsguedes@hotmail.com>
2024-07-08 19:22:33 -0300

    fix: removed unsupported icon

---
+++ TaskManager/Sources/Components/TaskView.swift
@@ -0,0 +1,28 @@
+//
+//  TaskView.swift
+//  TaskManager
+//
+//  Created by João Victor de Souza Guedes on 08/07/24.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                
+            }
+            
+            VStack {
+                Text("Title Task")
+                
+                Text("Hour Task")
+            }
+        }
+    }
+}
+
+#Preview {
+    TaskView()
+}
